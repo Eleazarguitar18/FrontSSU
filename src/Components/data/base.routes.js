96a51d695e5325d6dd4112545a67eb8d138ac9ce
Eleazarguitar18 <eleazarguitar18@gmail.com>
2024-04-11 12:39:02 -0400

    desarrollo

--- src/Components/data/base.routes.js
+++ src/Components/data/base.routes.js
@@ -1,7 +1,7 @@
 // export const url_base = "http://20.20.20.108:3000/api"
 // *url de Desarrollo
-// export const url_base = "http://localhost:3000/api";
+export const url_base = "http://localhost:3000/api";
 // export const url_base = "https://backendssudev.onrender.com/api"
 // export const url_base = " http:// 192.168.1.8:3000/api"
 // !url de produccion
-export const url_base = "https://proyecto-soporte-ssu.onrender.com/api";
+// export const url_base = "https://proyecto-soporte-ssu.onrender.com/api";
